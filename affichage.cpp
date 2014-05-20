dfe325770775754932b19fcb47c41ab2605619f7
MOPZ <paul.maupas@gmail.com>
2014-05-20 09:12:25 +0200

    Ajout API MySQL

---
+++ affichage.cpp
@@ -0,0 +1,20 @@
+void utilisateur_eleve()
+{
+    gotoxy(3.3);
+
+    cout << "Vous etes enregistré en temps qu'élève." << endl;
+}
+
+void utilisateur_prof()
+{
+    gotoxy(3,3);
+
+    cout << "Vous etes enregistré en temps que professeur." << endl;
+}
+
+void utilisateur_chef()
+{
+    gotoxy(3,3);
+
+    cout << "Vous etes enregistré en temps que chef de centre." << endl;
+}
